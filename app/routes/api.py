baecf77219fcbc81ee0c56791deb694de86e1062
Gravity <tevoned@gmail.com>
2023-01-03 20:50:39 -0500

    login features update

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+import sys
+from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
 
@@ -7,5 +8,22 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 
 @bp.route('/users', methods=['POST'])
 def signup():
+  data = request.get_json()
+  db = get_db()
 
-  return ''
+  try:
+    # attempt creating a new user
+    newUser = User(
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+    )
+
+    db.add(newUser)
+    db.commit()
+  except:
+    # insert failed, so send error to front end
+    print(sys.exe_info()[0])
+    return jsonify(message = 'Signup failed'), 500
+
+  return jsonify(id = newUser.id)
